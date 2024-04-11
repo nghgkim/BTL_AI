a9caf31358953f9751012acd1041997be84780c5
nghgkim <kim.nguyenhoang0412@gmail.com>
2024-04-11 15:32:16 +0700

    Update DFS Sudoku

--- sudoku/dfs_UI.py
+++ sudoku/dfs_UI.py
@@ -8,7 +8,7 @@ pygame.font.init()
 screen = pygame.display.set_mode((600, 700))
 
 # Title and Icon 
-pygame.display.set_caption("SUDOKU SOLVER USING BACKTRACKING")
+pygame.display.set_caption("SUDOKU SOLVER USING DFS")
 # img = pygame.image.load('icon.png')
 # pygame.display.set_icon(img)
 
@@ -16,19 +16,8 @@ x = 0
 y = 0
 dif = 500 / 9
 val = 0
-# Default Sudoku Board.
-# grid = [
-#     [7, 8, 0, 4, 0, 0, 1, 2, 0],
-#     [6, 0, 0, 0, 7, 5, 0, 0, 9],
-#     [0, 0, 0, 6, 0, 1, 0, 7, 8],
-#     [0, 0, 7, 0, 4, 0, 2, 6, 0],
-#     [0, 0, 1, 0, 5, 0, 9, 3, 0],
-#     [9, 0, 4, 0, 6, 0, 0, 0, 5],
-#     [0, 7, 0, 3, 0, 0, 0, 1, 2],
-#     [1, 2, 0, 0, 0, 7, 4, 0, 0],
-#     [0, 4, 9, 2, 0, 6, 0, 0, 7]
-# ]
-
+step = 0
+# Default Sudoku Board
 grid = [
 	[0,0,3,5,1,0,6,2,8],
     [2,9,0,0,0,3,7,0,0],
@@ -47,9 +36,9 @@ font2 = pygame.font.SysFont("comicsans", 20)
 
 def get_cord(pos):
 	global x
-	x = pos[0]//dif
+	x = pos[0] // dif
 	global y
-	y = pos[1]//dif
+	y = pos[1] // dif
 
 # Highlight the cell selected
 def draw_box():
@@ -69,6 +58,7 @@ def draw():
 				# Fill grid with default numbers specified
 				text1 = font1.render(str(grid[j][i]), 1, (0, 0, 0))
 				screen.blit(text1, (i * dif + 15, j * dif))
+				
 	# Draw lines horizontally and verticallyto form grid		 
 	for i in range(10):
 		if i % 3 == 0 :
@@ -90,57 +80,74 @@ def raise_error1():
 def raise_error2():
 	text1 = font1.render("Wrong !!! Not a valid Key", 1, (0, 0, 0))
 	screen.blit(text1, (20, 570)) 
+	
+def checkValid(board, num , pos):
+    # Check column
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+        
+    # Check row
+    for i  in range(len(board)):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False 
+        
+    # Check 3x3 box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+    
+    for i in range(box_y * 3, box_y * 3 + 3):
+         for j in range(box_x * 3, box_x * 3 + 3):
+             if board[i][j] == num and (i,j) != pos:
+                 return False
+             
+    return True
 
-# Check if the value entered in board is valid
-def valid(m, i, j, val):
-	for it in range(9):
-		if m[i][it]== val:
-			return False
-		if m[it][j]== val:
-			return False
-	it = i//3
-	jt = j//3
-	for i in range(it * 3, it * 3 + 3):
-		for j in range (jt * 3, jt * 3 + 3):
-			if m[i][j]== val:
-				return False
-	return True
+def findEmpty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)
+    
+    return None
 
 # Solves the sudoku board using Backtracking Algorithm
 def solve(grid, i, j):
-	
-	while grid[i][j]!= 0:
-		if i<8:
-			i+= 1
-		elif i == 8 and j<8:
-			i = 0
-			j+= 1
-		elif i == 8 and j == 8:
-			return True
+	global step
+	empty = findEmpty(grid)
+
+	if not empty:
+		return True
+	else: 
+		row, col = empty
+
 	pygame.event.pump() 
-	for it in range(1, 10):
-		if valid(grid, i, j, it)== True:
-			grid[i][j]= it
+
+	for i in range(1, 10):
+		if checkValid(grid, i, empty) == True:
+			grid[row][col]= i
 			global x, y
-			x = i
-			y = j
-			# white color background\
+			x = col
+			y = row
+
+			# white color background
 			screen.fill((255, 255, 255))
 			draw()
 			draw_box()
 			pygame.display.update()
-			pygame.time.delay(20)
-			if solve(grid, i, j)== 1:
+			pygame.time.delay(1000)
+			if solve(grid, row, col) == 1:
 				return True
 			else:
-				grid[i][j]= 0
-			# white color background\
+				grid[row][col] = 0
+
+			# white color background
 			screen.fill((255, 255, 255))
-		
+
 			draw()
 			draw_box()
 			pygame.display.update()
-			pygame.time.delay(50) 
+			pygame.time.delay(100) 
 	return False
 
 # Display instruction for the game
@@ -154,11 +161,13 @@ def instruction():
 def result():
 	text1 = font1.render("FINISHED PRESS R or D", 1, (0, 0, 0))
 	screen.blit(text1, (20, 570)) 
+	
 run = True
 flag1 = 0
 flag2 = 0
 rs = 0
 error = 0
+
 # The loop thats keep the window running
 while run:
 	# White color background
@@ -233,26 +242,27 @@ while run:
 				error = 0
 				flag2 = 0
 				grid = [
-					[7, 8, 0, 4, 0, 0, 1, 2, 0],
-					[6, 0, 0, 0, 7, 5, 0, 0, 9],
-					[0, 0, 0, 6, 0, 1, 0, 7, 8],
-					[0, 0, 7, 0, 4, 0, 2, 6, 0],
-					[0, 0, 1, 0, 5, 0, 9, 3, 0],
-					[9, 0, 4, 0, 6, 0, 0, 0, 5],
-					[0, 7, 0, 3, 0, 0, 0, 1, 2],
-					[1, 2, 0, 0, 0, 7, 4, 0, 0],
-					[0, 4, 9, 2, 0, 6, 0, 0, 7]
+					[0,0,3,5,1,0,6,2,8],
+                    [2,9,0,0,0,3,7,0,0],
+                    [0,0,0,0,4,0,0,0,5],
+                    [0,4,7,0,3,8,2,0,0],
+                    [5,1,2,6,0,7,0,4,0],
+                    [6,0,0,0,0,5,0,9,0],
+                    [7,8,0,0,0,0,0,6,0],
+                    [0,0,6,9,0,0,5,0,1],
+                    [1,0,4,2,7,0,3,0,0]
 				]
     
 	if flag2 == 1:
-		if solve(grid, 0, 0)== False:
+		if solve(grid, 0, 0) == False:
 			error = 1
 		else:
 			rs = 1
 		flag2 = 0
+		
 	if val != 0:		 
 		draw_val(val)
-		if valid(grid, int(x), int(y), val) == True:
+		if checkValid(grid, val, (int(x), int(y))) == True:
 			grid[int(x)][int(y)]= val
 			flag1 = 0
 		else:
@@ -277,4 +287,4 @@ while run:
 	pygame.display.update() 
 
 # Quit pygame window 
-# pygame.quit()
+pygame.quit()
